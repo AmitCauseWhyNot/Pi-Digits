3ccfe22d764139507a36339dc56fb7ba25c4c19a
AmitCauseWhyNot <amiteti77@gmail.com>
2025-02-23 09:26:09 +0200

    Added toggle for title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,20 +1,27 @@
-import { Text } from "theme-ui";
+import { ThemeProvider, Text, useColorMode } from "theme-ui";
 import { FormattedMessage } from "react-intl";
+import { theme } from "../common/theme";
 
 const Title = () => {
+  // Get the current color mode from Theme UI
+  const [colorMode] = useColorMode();
+
   return (
-    <Text
-      sx={{
-        color: "var(--main-text-color)",
-        fontStyle: "normal",
-        fontSize: "50px",
-        fontWeight: "bold",
-      }}
-    >
-      <FormattedMessage id="title" />
-      {/* Display the first n-digits of Pi */}
-    </Text>
+    <ThemeProvider theme={theme}>
+      <Text
+        sx={{
+          color: colorMode === "dark" ? "white" : "black", // Set color based on color mode
+          fontStyle: "normal",
+          fontSize: "50px",
+          fontWeight: "bold",
+        }}
+      >
+        <FormattedMessage id="title" />
+        {/* Display the first n-digits of Pi */}
+      </Text>
+    </ThemeProvider>
   );
 };
 
 export default Title;
+
